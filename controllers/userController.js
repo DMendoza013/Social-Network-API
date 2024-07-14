0dea28c911d6350a728f8c2b11cd47e0f8ff8a7f
DMendoza013 <damien.mendoza013@gmail.com>
2024-07-14 14:51:11 -0700

    added post, put, and delete routes for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,17 +25,42 @@ async getSingleUser(req, res) {
 },
 
 async createUser(req, res) {
-
+    try {
+        const newUser = user.create(req.body);
+        res.json(body);
+    }catch(err) {
+        res.status(500).json(err);
+    }
 },
 
 async updateUser(req, res) {
-    
+    try {
+        const updateUser = await user.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new:true }
+        );
+
+    if (!updateUser){
+        res.status(400).json({ message: 'No user with this ID'})
+    }
+    }catch(err) {
+        res.status(500).json(err);
+    } 
 },
 
 async deleteUser(req, res) {
+    try {
+        const deleteUser = await user.findOneAndRemove({ _id: req.params.userId});
 
+        if (!deleteUser) {
+            res.status(404).json({ message: 'No user with this ID'})
+        }
+
+        res.json({ message: 'User has been deleted' });
+    } catch(err) {
+        res.status(500).json(err);
+    }
 },
-
-
 
 }
